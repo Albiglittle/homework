7a8e39651984598ddf3e7f03f38db1f08f0b89bf
Temiyon <te>
2015-01-12 03:30:40 +0300

    .

--- Builder/.builder/checkfailed.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-if "%builder_starts%"=="" goto :EOF
-
-echo Missed %file_missed%.>%search_log%
-if exist %attache% echo ,>%attache% 
-echo %search_log%>>%attache%
-
-set email_extra_subject=BUILD FAILED
